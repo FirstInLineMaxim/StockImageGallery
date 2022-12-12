eef28511cb5a4715d71ab685e5d331228e1990b9
Alessandra Maffei <ales.maffei@gmail.com>
2022-12-12 16:01:32 +0100

    download button

--- src/components/datafetch/imagefetch.jsx
+++ src/components/datafetch/imagefetch.jsx
@@ -5,35 +5,33 @@ import domtoimage from "dom-to-image";
 import "../popup/Popup.css";
 import { saveAs } from "file-saver";
 
+export default function ImageFetch({ query }) {
+  const [openPopup, setOpenPopup] = useState(false);
+  const [imageArray, setimageArray] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(20);
+  const [imageIndex, setImageIndex] = useState();
 
-export default function ImageFetch({query}){
-    const [openPopup,setOpenPopup] = useState(false)
-    const [imageArray,setimageArray] = useState([])
-    const [currentPage,setCurrentPage] =useState(1)
-    const [postsPerPage,setPostsPerPage] = useState(20)
-    const [imageIndex,setImageIndex] = useState()
-
-    useEffect(()=>{
-        async function fetchData(){
-       await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=80`,{
-            headers:{
-
-                Authorization: "563492ad6f917000010000018d6e567481954be7adb58821c258f84b"
-
-            }
-        })
-        .then((res)=>res.json())
-        .then((data)=>setimageArray(data.photos))
+  useEffect(() => {
+    async function fetchData() {
+      await fetch(
+        `https://api.pexels.com/v1/search?query=${query}&per_page=80`,
+        {
+          headers: {
+            Authorization:
+              "563492ad6f917000010000018d6e567481954be7adb58821c258f84b",
+          },
         }
-        fetchData()
-        
-    },[query])
-    
-    function popup(e){ 
-        
-        
-        setOpenPopup(true)
-      }
+      )
+        .then((res) => res.json())
+        .then((data) => setimageArray(data.photos));
+    }
+    fetchData();
+  }, [query]);
+
+  function popup(e) {
+    setOpenPopup(true);
+  }
 
   const saveImgHandler = () => {
     domtoimage.toBlob(document.getElementById("my-node")).then(function (blob) {
@@ -41,51 +39,83 @@ export default function ImageFetch({query}){
     });
   };
 
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = imageArray.slice(firstPostIndex, lastPostIndex);
 
-    const lastPostIndex= currentPage * postsPerPage
-    const firstPostIndex = lastPostIndex - postsPerPage
-    const currentPosts =  imageArray.slice(firstPostIndex,lastPostIndex)
-   
-    const  imageMap= currentPosts.map((i,index)=>{
+  const imageMap = currentPosts.map((i, index) => {
+    return (
+      <>
+        <button
+          style={{ background: "none", border: "0px" }}
+          onClick={() => {
+            setOpenPopup(true);
+            setImageIndex(i);
+            console.log(i);
+          }}
+        >
+          {" "}
+          <figure>
+            {" "}
+            <img src={i.src.tiny} className="single-image" />
+          </figure>
+        </button>
+      </>
+    );
+  });
 
-       return(
-        <>
-       <button  style={{background: 'none',border:'0px'}} onClick={()=>{
-        setOpenPopup(true)
-        setImageIndex(i)
-        console.log(i)
-       }}> <figure > <img  src={i.src.tiny} className="single-image" /></figure></button>
-   
-     </>
-       )
-    })
-    
-    if(imageArray&&imageIndex){
-    return(
-        <>
-       <div className="media-container">
-            <span className="media-grid"> {imageMap} </span>
-        </div>
-    <Popup open={openPopup} onClose={()=>setOpenPopup(false)}>
-        <div style={{width:'1000px',height:'600px',background: 'white',border:'0px'}} >
-        <h1>{imageIndex.photographer}</h1>
-        <img src={imageIndex.src.medium}/>
-        </div>
-    </Popup>
-
-       <Pagination totalPosts={imageArray.length} postsPerPage={postsPerPage} setCurrentPage={setCurrentPage}/>
-
-        </>
-    )
-}
-
-    return(
-        <>
+  if (imageArray && imageIndex) {
+    return (
+      <>
         <div className="media-container">
-            <span className="media-grid"> {imageMap} </span>
+          <span className="media-grid"> {imageMap} </span>
         </div>
-           <Pagination totalPosts={imageArray.length} postsPerPage={postsPerPage} setCurrentPage={setCurrentPage}/>
-            </>
-    )
-}
+        <Popup open={openPopup} onClose={() => setOpenPopup(false)}>
+          <div
+            style={{
+              width: "1000px",
+              height: "600px",
+              background: "white",
+              border: "0px",
+              paddingLeft: "5%",
+            }}
+          >
+            <div className="popup-info-cont">
+              <h2 className="popup-author">{imageIndex.photographer}</h2>
+              <button
+                className="popup-bttn"
+                onClick={() => {
+                  saveImgHandler();
+                }}
+              >
+                Free download
+              </button>
+            </div>
+            <div id="my-node">
+              <img className="popup-img" src={imageIndex.src.medium} />
+            </div>
+          </div>
+        </Popup>
 
+        <Pagination
+          totalPosts={imageArray.length}
+          postsPerPage={postsPerPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="media-container">
+        <span className="media-grid"> {imageMap} </span>
+      </div>
+      <Pagination
+        totalPosts={imageArray.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </>
+  );
+}
