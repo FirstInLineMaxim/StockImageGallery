41420c2c6894034597a6c55568a856936577a3cf
Alessandra Maffei <ales.maffei@gmail.com>
2022-12-12 13:13:57 +0100

    header search styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { Link } from 'react-router-dom';
-import './App.css';
-import CustomRoutes from './components/router/Router';
+import { Link } from "react-router-dom";
+import "./App.css";
+import CustomRoutes from "./components/router/Router";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-    <h1>Plexfull</h1>
-    {/* HEADER */}
-    <Link to="/">Home</Link>
-    <br/>
-    <Link to="/images">Images</Link>
-    <br/>
-    <Link to='/video'>Video</Link>
+      <Header />
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/images">Images</Link>
+      <br />
+      <Link to="/video">Video</Link>
 
-    {/* SEARCH */}
+      {/* SEARCH */}
 
-    {/* ROUTER */}
-    <CustomRoutes/>
+      {/* ROUTER */}
+      <CustomRoutes />
     </>
   );
 }
