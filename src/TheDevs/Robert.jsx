5660d9599b39e4dcced7af08013b600eb30fe0ce
FirstInLineMaxim <109926706+FirstInLineMaxim@users.noreply.github.com>
2022-12-12 15:56:10 +0100

    all good

---
+++ src/TheDevs/Robert.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+
+export default function Robert() {
+    const ref = useRef();
+  return (
+    <Flippy
+    flipOnHover={false} // default false
+    flipOnClick={true} // default false
+    flipDirection="horizontal" // horizontal or vertical
+    ref={ref} // to use toggle method like ref.curret.toggle()
+    // if you pass isFlipped prop component will be controlled component.
+    // and other props, which will go to div
+    style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
+>
+    <FrontSide style={{ backgroundColor: '#f5f5f5' }} >
+    <h2>Robert</h2>
+    <h3>Fullstack Developer For Hire</h3>
+    <i>Digital Prophet</i>
+    <br/>
+    <h4>Click Me</h4>
+
+    </FrontSide>
+    <BackSide style={{ backgroundColor: '#f5f5f5' }}>
+      <a href='https://github.com/SanJunipero1'>
+      <img  className="gitlogo" src='https://pngimg.com/uploads/github/github_PNG15.png'></img>
+      github.com/SanJunipero1
+      </a>
+    </BackSide>
+</Flippy>
+  )
+}
--- src/TheDevs/TheDevs.jsx
+++ src/TheDevs/TheDevs.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import Ada from './Ada'
+import Alessandra from './Alessandra'
+
 import './devs.css'
+import Maksim from './Maksim'
+import Robert from './Robert'
+
 
 export default function TheDevs() {
-  return (
-    <div className='theMain'>
+
+    return (
+        <div className='theMain'>
             <h3 className="h3">The Developer of this Project</h3>
-        <div className='theDevs'>
+            <div className='theDevs'>
+                < Alessandra/>
+                <Maksim/>
+                <Ada/>
+                <Robert/>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
